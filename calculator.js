c3ce496ca41a8e9b23c7138263a0822426d0dbea
nrbhagwat <46270085+nrbhagwat@users.noreply.github.com>
2019-01-09 14:46:50 -0500

    Revert "Js calculator"

--- calculator.js
+++
@@ -1,8 +0,0 @@
-function multiply(x,y) {return x * y}
-function add(x,y) {return x + y}
-function subtract(x,y) {return x - y}
-function divide(x,y) {return x / y}
-
-console.log("I'm going to use the calculator functions to multiply 5 and 6")
-var x = multiply(5,6)
-console.log(x)
