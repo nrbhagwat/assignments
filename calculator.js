262a8ce51c58ed6f6252e644969f701acb04b362
nrbhagwat <nikhil_bhagwat@student.hks.harvard.edu>
2019-01-09 14:45:18 -0500

    call the function to multiply 5 and 6

--- calculator.js
+++ calculator.js
@@ -3,3 +3,6 @@ function add(x,y) {return x + y}
 function subtract(x,y) {return x - y}
 function divide(x,y) {return x / y}
 
+console.log("I'm going to use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
