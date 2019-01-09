fd0b4ec342daf4f679dcc8f47c97b8b9ff986826
nrbhagwat <nikhil_bhagwat@student.hks.harvard.edu>
2019-01-09 14:54:46 -0500

    added square_n_times

--- calculator.py
+++ calculator.py
@@ -10,6 +10,8 @@ def square(a):
 	return a**2
 def cube(a):
 	return a**3
+def square_n_times(a,b):
+	return a**b
 
 print("I'm going to use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
