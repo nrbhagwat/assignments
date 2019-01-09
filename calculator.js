7fe320659491629b8e61bae0f30f36feb4401b71
nrbhagwat <nikhil_bhagwat@student.hks.harvard.edu>
2019-01-09 14:43:14 -0500

    add functions to calculator

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
-function multiply(x,y) {x * y}
-function add(x,y) {x + y}
-function subtract(x,y) {x - y}
-function divide(x,y) {x / y}
+function multiply(x,y) {return x * y}
+function add(x,y) {return x + y}
+function subtract(x,y) {return x - y}
+function divide(x,y) {return x / y}
+
