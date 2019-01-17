5f8398d183d8cec4caab8dc590271755733df943
nrbhagwat <nikhil_bhagwat@student.hks.harvard.edu>
2019-01-17 11:25:52 -0500

    add python CSV lesson

---
+++ pythondata/veggies.py
@@ -0,0 +1,26 @@
+import csv
+
+vegetables = [
+ {"name": "eggplant", "color": "purple"},
+ {"name": "tomato", "color": "red"},
+ {"name": "corn", "color": "yellow"},
+ {"name": "okra", "color": "green"},
+ {"name": "arugula", "color": "green"},
+ {"name": "broccoli", "color": "green"},
+]
+
+#write the vegetable to a CSV file
+
+
+with open('vegetables.csv', 'w') as f:
+	writer = csv.writer(f)
+	#Write header
+	writer.writerow(['name', 'color', 'length'])
+	#Write data
+  
+	 #loop through each vegetable
+	for vegetable in vegetables:
+		name = vegetable['name']
+		color = vegetable['color']
+		length = len(vegetable['name'])
+		writer.writerow([name,color])
